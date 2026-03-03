4b6be2bbd694ccd228d0598710bac10f60aafb34
Maciej Ornowski <orno.morsoft@gmail.com>
2026-03-03 20:32:22 +0100

    folder structure for ./messages and first translation in Footer

--- features/sign-out-layout/components/Footer.tsx
+++ features/sign-out-layout/components/Footer.tsx
@@ -5,15 +5,21 @@ import { useTranslations } from 'next-intl'
 import { Divider, TextLink } from '@/components'
 
 const Footer = () => {
-const t = useTranslations('Footer')
+  const t = useTranslations('Footer')
   return (
     <div className='py-6.5 shadow-footer flex w-full items-center justify-between bg-dark-brand px-10 text-white'>
       <div className='flex items-center gap-x-10'>
-        <p>Devstock.pl</p>
+        <TextLink href='/'>
+          <span>Devstock.pl</span>
+        </TextLink>
         <Divider />
-        <TextLink href='/'>{t('privacyPolicy')}</TextLink>
+        <TextLink href='/'>
+          <span>{t('privacy')}</span>
+        </TextLink>
         <Divider />
-        <TextLink href='/'>{t('contact')}</TextLink>
+        <TextLink href='/'>
+          <span>{t('contact')}</span>
+        </TextLink>
       </div>
       <p>💛 Devstock © 2023</p>
     </div>
