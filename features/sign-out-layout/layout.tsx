8464e576e33ea97a5910e779e754865b395840ee
Maciej Ornowski <orno.morsoft@gmail.com>
2026-03-06 22:42:49 +0100

    sign-out-topbar layout

---
+++ features/sign-out-layout/layout.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import {
+  SignOutTopbar,
+  SocialMediaBar,
+  Footer,
+} from '@/features/sign-out-layout/components'
+
+type SignOutLayoutProps = {
+  children: React.ReactNode
+}
+
+const SignOutLayout = ({ children }: SignOutLayoutProps) => {
+  return (
+    <div className='flex min-h-screen flex-col'>
+      <SocialMediaBar />
+      <SignOutTopbar />
+      <main className='flex-1'>{children}</main>
+      <Footer />
+    </div>
+  )
+}
