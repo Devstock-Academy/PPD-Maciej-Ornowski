78368ea4e6de3e0be632c9b407789e49cb7347bc
Maciej Ornowski <orno.morsoft@gmail.com>
2026-03-06 22:36:24 +0100

    component sign-out-topbar + button + navbutton + logo + navtext

--- components/nav-button.tsx
+++ components/nav-button.tsx
@@ -1,6 +1,35 @@
-const NavButton = () => {
+import React from 'react'
+import Link from 'next/link'
+
+import { Button } from '@/components'
+
+type NavButtonProps = {
+  children: React.ReactNode
+  href?: string
+  size?: 'sm' | 'md' | 'lg'
+  variant?:
+    | 'primary'
+    | 'alternative'
+    | 'alternative-dark'
+    | 'dark'
+    | 'gray'
+    | 'green'
+    | 'red'
+    | 'white'
+}
+
+const NavButton = ({
+  href = '/',
+  children,
+  size = 'sm',
+  variant = 'primary',
+}: NavButtonProps) => {
   return (
-    <></>
+    <Link href={href}>
+      <Button onClick={() => {}} size={size} className='px-8.125'>
+        {children}
+      </Button>
+    </Link>
   )
 }
 
--- features/sign-out-layout/components/sign-out-topbar.tsx
+++ features/sign-out-layout/components/sign-out-topbar.tsx
@@ -1,3 +1,30 @@
-const SignOutTopbar = () => <div>SignOutTopbar</div>
+'use client'
+
+import { useTranslations } from 'next-intl'
+import Link from 'next/link'
+
+import { NavButton, Divider, TextLink } from '@/components'
+import DevstockLogo from '@/icons/devstock-logo'
+
+const SignOutTopbar = () => {
+  const t = useTranslations('Footer')
+  return (
+    <div className=' flex h-20 w-full items-center justify-between bg-dark-brand px-10 '>
+      <Link href='/'>
+        <DevstockLogo />
+      </Link>
+      <div className=' flex items-center justify-evenly gap-x-10 '>
+        <TextLink href='/'>
+          <span>Devstock.pl</span>
+        </TextLink>
+        <Divider />
+        <TextLink href='/'>
+          <span>{t('login')}</span>
+        </TextLink>
+        <NavButton>{t('sign-up')}</NavButton>
+      </div>
+    </div>
+  )
+}
 
 export default SignOutTopbar
