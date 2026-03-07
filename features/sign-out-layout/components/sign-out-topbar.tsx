6889ddf67b4668e4ad95bf82e514123b9b2e9a48
Maciej Ornowski <orno.morsoft@gmail.com>
2026-03-07 09:48:12 +0100

    Devstock-Logo and Devstock-Text @icons refactored Devstock-Academy-Logo @sign-out-topbar

--- features/sign-out-layout/components/sign-out-topbar.tsx
+++ features/sign-out-layout/components/sign-out-topbar.tsx
@@ -4,14 +4,14 @@ import { useTranslations } from 'next-intl'
 import Link from 'next/link'
 
 import { NavButton, Divider, TextLink } from '@/components'
-import DevstockLogo from '@/icons/devstock-logo'
+import { DevstockAcademyLogo } from '@/icons'
 
 const SignOutTopbar = () => {
   const t = useTranslations('TopBar')
   return (
     <div className=' flex h-20 w-full items-center justify-between bg-dark-brand px-10 '>
       <Link href='/'>
-        <DevstockLogo />
+        <DevstockAcademyLogo />
       </Link>
       <div className=' flex items-center justify-evenly gap-x-10 '>
         <TextLink href='/'>
