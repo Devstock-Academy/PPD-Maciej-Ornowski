c7ceb9bc22158dd73ded4e730012f140ddfee3b1
Maciej Ornowski <orno.morsoft@gmail.com>
2026-03-11 00:37:54 +0100

    register-form iteration #3 - first validation

--- features/register/components/register-form.tsx
+++ features/register/components/register-form.tsx
@@ -2,38 +2,32 @@ import { Button, Checkbox, Label, TextInput } from 'flowbite-react'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import { useForm } from 'react-hook-form'
+import React from 'react'
+import clsx from 'clsx'
 
 const formSchema = z
   .object({
-    pseudonim: z
-      .string()
-      .min(1, 'Pseudonim musi zawierać odpowiednią ilość znaków'),
-    imie: z.string().min(1, 'Imię musi zawierać odpowiednią ilość znaków'),
-    nazwisko: z
-      .string()
-      .min(1, 'Nazwisko musi zawierać odpowiednią ilość znaków'),
-    email: z
-      .string()
-      .min(1, 'Email jest wymagany')
-      .email('E-mail musi mieć poprawny format'),
+    pseudonim: z.string().min(3, 'Wymagane przynajmniej 3 znaki'),
+    imie: z.string().min(3, 'Wymagane przynajmniej 3 znaki'),
+    nazwisko: z.string().min(3, 'Wymagane przynajmniej 3 znaki'),
+    email: z.email('Niepoprawny adres e-mail').min(1, 'Email jest wymagany'),
     password: z
       .string()
-      .min(8, 'Hasło musi zawierać odpowiednią ilość znaków')
-      .regex(/[A-Z]/, 'Hasło musi zawierać co najmniej jedną dużą literę')
-      .regex(/[a-z]/, 'Hasło musi zawierać co najmniej jedną małą literę')
-      .regex(/[0-9]/, 'Hasło musi zawierać co najmniej jedną liczbę')
-      .regex(
-        /[!@#$%^&*(),.?":{}|<>]/,
-        'Hasło musi zawierać co najmniej jeden znak specjalny'
-      ),
-    confirmPassword: z.string().min(1, 'Hasło jest niezgodne'),
+      .min(8, 'Minimum 8 znaków')
+      .regex(/[A-Z]/, 'Przynajmniej 1 duża litera')
+      .regex(/[a-z]/, 'Przynajmniej 1 mała litera')
+      .regex(/[0-9]/, 'Przynajmniej 1 liczba')
+      .regex(/[!@#$%^&*(),.?":{}|<>]/, 'Przynajmniej 1 znak specjalny'),
+    confirmPassword: z
+      .string()
+      .min(1, 'Musi zawierać dokładnie to, co pole hasło.'),
     rulesAccepted: z.boolean().refine((val) => val === true, {
-      message: 'Musisz zaakceptować zasady i warunki świadczenia usług!',
+      message: 'Musi być zaznaczony!',
     }),
   })
   .refine((data) => data.password === data.confirmPassword, {
     path: ['confirmPassword'],
-    message: 'Hasło jest niezgodne',
+    message: 'Musi zawierać dokładnie to, co pole hasło.',
   })
 
 type FormData = z.infer<typeof formSchema>
@@ -49,6 +43,17 @@ const RegisterForm = () => {
     mode: 'onBlur',
   })
 
+  const [submitted, setSubmitted] = React.useState(false)
+  const [showModal, setShowModal] = React.useState(false)
+  const [email, setEmail] = React.useState('')
+
+  const onSubmit = async (data: FormData) => {
+    setEmail(data.email)
+    reset()
+    setSubmitted(true)
+    setShowModal(true)
+  }
+
   return (
     <div className='flex w-175 flex-col items-center rounded-lg bg-dark-brand p-8 drop-shadow-lg'>
       <form className='flex w-full flex-col gap-y-8'>
@@ -61,12 +66,15 @@ const RegisterForm = () => {
               Pseudonim
             </Label>
             <TextInput
-              id='email1'
-              type='email'
-              placeholder='name@flowbite.com'
-              required
-              color='gray'
+              id='pseudonim'
+              type='text'
+              placeholder='Pseudonim'
+              {...register('pseudonim')}
+              className={clsx('border border-gray-600 bg-gray-700', {
+                'border-[#F74746]': errors.pseudonim,
+              })}
             />
+            {errors.pseudonim && <p>{errors.pseudonim.message}</p>}
           </div>
           <div className='w-75'>
             <Label className='text-white' htmlFor='name1'>
