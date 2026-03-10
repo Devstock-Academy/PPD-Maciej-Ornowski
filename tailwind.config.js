7ca9779f3c26a2a0071f0803b5d497687cfed1d0
Maciej Ornowski <orno.morsoft@gmail.com>
2026-03-10 16:28:10 +0100

    tailwind config prepared in P#1

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,42 @@ module.exports = {
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#121415',
+        primary: {
+          DEFAULT: '#0074F0',
+          600: '#0069D9',
+          800: '#0056B3',
+        },
+        'dark-brand': '#222426',
+        'rich-black': '#121415',
+        'hero-overlay': '#1B2124',
+      },
+      spacing: {
+        2.5: '0.625rem', // 10px
+        3.5: '0.875rem', // 14px
+        4.5: '1.125rem', // 18px
+        6.5: '1.625rem', // 26px
+        8.125: '2.03125rem', // 32.5px
+        8.1875: '2.0625rem', // 33px
+        15: '3.75rem', // 60px
+        30: '7.5rem', // 120px
+        34: '8.5rem', // 136px
+        37.5: '9.375rem', // 150px
+        42.75: '10.6875rem', // 171px
+        45.5: '11.375rem', // 182px
+        47.5: '11.875rem', // 190px
+        50: '12.5rem', // 200px
+        72: '18rem', // 288px
+        120: '30rem', // 480px
+        197.5: '49.375rem', // 790px
+      },
+      maxWidth: {
+        content: '85.375rem',
+        video: '40.1875rem',
+      },
+    },
   },
   plugins: [require('flowbite/plugin')],
 }
