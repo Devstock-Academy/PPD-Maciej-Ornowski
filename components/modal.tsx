a96fdfe3cd6f934fe0c6e8cb55ac37d87f04d5f1
Maciej Ornowski <orno.morsoft@gmail.com>
2026-03-11 15:48:44 +0100

    modal iteration #1

--- components/modal.tsx
+++ components/modal.tsx
@@ -1,11 +1,8 @@
-const Modal = () => {
-  return (
-    <div
-      data-testid='modal'
-    >
+import { useTranslations } from 'next-intl'
 
-    </div>
-  )
+const Modal = () => {
+  const t = useTranslations('Modal')
+  return <div>Modal</div>
 }
 
 export default Modal
