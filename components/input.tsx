6a9eab8fad87f779378965e042eebd3deca7b152
Maciej Ornowski <orno.morsoft@gmail.com>
2026-03-13 09:25:06 +0100

    input wrapped as component iteration #1

--- components/input.tsx
+++ components/input.tsx
@@ -1,6 +1,24 @@
-const Input = () => {
+import React from 'react'
+import { HelperText, Label, TextInput } from 'flowbite-react'
+
+type InputProps = {
+  label: string
+  error?: string
+}
+
+const Input: React.FC<InputProps> = ({ label, error, ...props }) => {
   return (
-    <></>
+    <div className='w-75'>
+      <Label className='font-medium leading-normal text-white' htmlFor={label}>
+        {label}
+      </Label>
+      <TextInput id={label} placeholder={label} {...props} />
+      {error && (
+        <HelperText className='text-xs font-extralight leading-normal text-[#F74746]'>
+          {error}
+        </HelperText>
+      )}
+    </div>
   )
 }
 
