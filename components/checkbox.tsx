a0a2c15755cd95420287a15bb2ac7c69b84d217a
Maciej Ornowski <orno.morsoft@gmail.com>
2026-03-14 22:10:49 +0100

    checkbox wrapped as component iteration #1

--- components/checkbox.tsx
+++ components/checkbox.tsx
@@ -1,7 +1,31 @@
-const Checkbox = () => {
-  return (
-     <></>
+import React, { forwardRef } from 'react'
+import { HelperText, Label, Checkbox as CheckboxFlowbite } from 'flowbite-react'
+
+type CheckboxProps = {
+  label: string
+  error?: string | undefined
+} & React.ComponentProps<typeof CheckboxFlowbite>
+
+const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ label, error, ...props }, ref) => {
+    return (
+      <div className='flex flex-col'>
+        <div className='flex items-center gap-x-4'>
+          <CheckboxFlowbite ref={ref} {...props} />
+          <Label className='text-white' htmlFor={label}>
+            {label}
+          </Label>
+        </div>
+        {error && (
+          <HelperText className='text-xs font-extralight leading-normal text-red-brand'>
+            {error}
+          </HelperText>
+        )}
+      </div>
     )
-}
+  }
+)
+
+Checkbox.displayName = 'Checkbox'
 
 export default Checkbox
